13c486733d77cb973c57cf807cc4a1e71f90f6ad
auyer <rafael@rcpassos.me>
2023-10-11 10:22:28 -0300

    documentation on header calls

--- sdks/c/libMemoryKV/libMemoryKV.h
+++ sdks/c/libMemoryKV/libMemoryKV.h
@@ -1,5 +1,7 @@
 #include <stdbool.h>
 
+// memkv_client stores the configurations used to connect to the MemoryKV server
+// use `memkv_client_new` to create one, or `memkv_init_client` to initialize an existing instance
 typedef struct memkv_client {
 	char *host;
 } memkv_client;
@@ -16,14 +18,20 @@ typedef struct {
 	};
 } memkv_result;
 
-void memkv_init_client(struct memkv_client *client, char *host);
-
+// memkv_client_new creates a new memkv_client instance with a host url pre filled
 memkv_client *memkv_client_new(char *host);
 
+// memkv_init_client initializes an existing memkv_client with a provided URL
+void memkv_init_client(struct memkv_client *client, char *host);
+
+// memkv_get_key fetches the value of a key from the MemoryKV server
 memkv_result *memkv_get_key(struct memkv_client *client, const char *key);
 
+// memkv_delete_key deletes a key from the MemoryKV server, and return a value if it was deleted
 memkv_result *memkv_delete_key(struct memkv_client *client, const char *key);
 
+// memkv_put_key puts a key value pair in the MemoryKV server, and return a value if there was one in the key beafore
 memkv_result *memkv_put_key(struct memkv_client *client, const char *key, const char *put_body);
 
+// memkv_list_keys lists all the keys in the MemoryKV server
 memkv_result *memkv_list_keys(struct memkv_client *client);
